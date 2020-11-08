8ba018d293586da9fc6d114c7fb5a60563606832
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-08 18:59:47 -0300

    Resetting a Form

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -38,4 +38,8 @@ export class CardFormComponent implements OnInit {
   onSubmit(): void {
     console.log('Form was submitted');
   }
+
+  onResetClick(): void {
+    this.cardForm.reset();
+  }
 }
