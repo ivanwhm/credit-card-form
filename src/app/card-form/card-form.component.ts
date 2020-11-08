b090c0007d9781d70908b8e2aae475d296d81a91
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-08 17:55:11 -0300

    Handling Form Submittion

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,12 +9,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    cardNumber: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    expiration: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    securityCode: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl(''),
+    expiration: new FormControl(''),
+    securityCode: new FormControl(''),
   });
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log('Form was submitted');
+  }
 }
